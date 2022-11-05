75f6739612d266ad73905c795c0230fe0b1ee5da
gab5987 <biell.gb4@gmail.com>
2022-11-05 19:25:33 -0300

    Feat: tokenizer class

--- asm/asm.h
+++ asm/asm.h
@@ -1,6 +1,20 @@
 #ifndef ASM_HELPER_H
 #define ASM_HELPER_H
+#include <vector>
+#include <iostream>
 
 int openFile(char* filename);
+
+static class Tokenizer {
+    private:
+        char* source;
+        int line;
+        int column;
+        int length;
+        char* text;
+        char* filename;
+    public:
+        void tokenize(std::string* line);
+} Tokenizer;
 
 #endif
